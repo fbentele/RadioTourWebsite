e6fff590ede89545490448bd3d905a9f2c675a86
Florian Bentele <fbentele@gmail.com>
2013-05-01 15:11:57 +0200

    RaceSituation modification

--- src/main/java/ch/hsr/ba/tourlive/model/rider/RaceSituation.java
+++ src/main/java/ch/hsr/ba/tourlive/model/rider/RaceSituation.java
@@ -14,7 +14,6 @@ import javax.persistence.Table;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import ch.hsr.ba.tourlive.model.Situation;
 import ch.hsr.ba.tourlive.model.Stage;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
